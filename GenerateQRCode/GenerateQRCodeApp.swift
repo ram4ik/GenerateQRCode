d2e109cc1a29db90a3d0186678d58ae1aec414ab
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2020-08-04 21:40:57 +0300

    Initial Commit

---
+++ GenerateQRCode/GenerateQRCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  GenerateQRCodeApp.swift
+//  GenerateQRCode
+//
+//  Created by Ramill Ibragimov on 04.08.2020.
+//
+
+import SwiftUI
+
+@main
+struct GenerateQRCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
